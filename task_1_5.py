3981dcdb7a742be7d10f5dfb70f880348e5b334b
DmitrenkoArtem <dmaracc0unt@gmail.com>
2024-11-19 19:19:21 +0000

    ...

--- task_1_5.py
+++ task_1_5.py
@@ -1,8 +1,15 @@
-# from task_1_4 import trigonometry_array as ta
-# import numpy as np
+from task_1_4 import trigonometry_array as ta
+import numpy as np
 
 # for i in range(ta.shape[0]):
 #     tmp=ta[i,0]
 #     ta[i,0]=ta[i,1]
 #     ta[i,1]=tmp
 # print(ta)
+
+n1=int(input('Первый столбец: '))
+n2=int(input('Второй столбец: '))
+tmp=np.array(ta[:,n1])
+ta[:,n1]=ta[:,n2]
+ta[:,n2]=tmp
+print(ta)
