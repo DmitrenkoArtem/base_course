a8255c6be74ea06e89a6bd9196cd77585419cc49
DmitrenkoArtem <dmaracc0unt@gmail.com>
2024-11-27 16:06:38 +0000

    gol

--- task_2_4.py
+++ task_2_4.py
@@ -2,10 +2,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def lestnitsa(n):
-    def add(m):
-        plt.plot([m,m,m+1],[m,m+1,m+1])
+    def add(h):
+        plt.plot([h,h,h+1],[h,h+1,h+1])
     for i in range(n):
         add(i)
-    plt.savefig('task_2_4.png')
+    plt.savefig('task_2_4_1.png')
+def lestnitsa2(n):
+    x=[]
+    y=[]
+    for i in range(n):
+        x.append(i)
+        y.append(i)
+        x.append(i)
+        y.append(i+1)
+        x.append(i+1)
+        y.append(i+1)
+    plt.plot(x,y)
+    plt.savefig('task_2_4_2.png')
+def lestnitsa3(n):
+    x=np.arange(0,n,0.1)
+    y=x//1
+    plt.plot(x,y)
+    plt.savefig('task_2_4_3.png')
 if __name__=='__main__':
-    lestnitsa(5)
+    #lestnitsa(10)
+    #lestnitsa2(10)
+    #lestnitsa3(10)
